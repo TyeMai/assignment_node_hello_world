e225e26f1c4166d9a88dcad1774a4685f228d52a
tyemai <tyeers@gmail.com>
2018-03-26 23:55:31 -0700

    lodash and chalk

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+
+var lodash = require('lodash');
+var chalk = require('chalk');
+
+var array2 = [1,2,3,4,5]
+
+var array = require('lodash/array');
+
+console.log(chalk.magenta('hello beautiful'))
+
+console.log(lodash.each(array))
