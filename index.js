42648f2be541ad666fff13f7c439623d6917b646
tyemai <tyeers@gmail.com>
2018-03-31 18:05:22 -0700

    alex corrections

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 
 var myLogger = require('./lib/logger');
-
-//console.log('hello world')
-//console.log(myLogger.name)
-
 var data = require('./data/logs.json');
-
 var _ = require('lodash');
 
 
 
-//myLogger.logger("info test","info")
-
-
-
-//myLogger.logger(_.each(data))
-
-//console.log(_.each(data.message)) undefined
-
-//console.log(data.message) undefined
-
-//console.log(_.each(data)) outputs logs.json file
-
-_.each(data, function(obj, index) {
-
-  //console.log(msg)
-  //console.log(lvl.level)
-
-  myLogger.logger(obj.message, obj.level)
-  //console.log(data)
+_.each(data, function(obj) {
+  myLogger.log(obj.message, obj.level)
 })
+//change to log.
